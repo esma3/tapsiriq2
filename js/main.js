86455209f7aa5c57e9fa5fd6f54e7f206867cb42
esma3 <semababayeva9898@gmail.com>
2022-10-24 02:24:21 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// var car ="BMW"
+// var car2="Mercedes"
+// var car3 ="Ford"
+// var car4 ="Kia"
+// var var5="Range Rover"
+// var car6 ="Chevrolet"
+
+
+// document.write(car)
+// document.write(car2)
+
+
+
+// var masinlar =['BMW','Mercedes','Ford','Kia','Range Rover','Chevrolet']
+// console.log("menim masinimin modeli "+masinlar[2]+" dir.")
+
+// console.log(masinlar[1][2])
+
+var seyahet =[
+    ["Tebiet",
+    ["Deniz","Hava Limani"]
+    ,"Ismayilli","Almanya"],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["Revan","Sebuhi","Rza","Deyanet"],
+    ["Shah dag",
+    ["Yay","Qis"],"Mandarin"
+    ],
+    ["","Bakı."]
+];
+
+
+
+// console.log(seyahet[2][1][0]+seyahet[0][0][4]
+// +seyahet[0][2][2]+seyahet[3][1][0][1]+seyahet[1][4][6]+seyahet[4][1])
+// console.log(seyahet[0][1][2]+" - "+seyahet[1][1]+" - "+seyahet[4][0])
+
+
+console.log(seyahet[3][2][0]+seyahet[0][0][1]+seyahet[2][0][4]+seyahet[0][2][8]+seyahet[1][4][8]+" "+seyahet[1][0][3]+seyahet[3][0][5]+seyahet[4][1][3]+
+seyahet[1][2][2]+" " +seyahet[2][1][0]+seyahet[0][1][0][1]+seyahet[0][3][2]+seyahet[1][3][3]+seyahet[3][0][5]+seyahet[4][1][3]+seyahet[3][2][5]+
+seyahet[4][1][4])
+document.write(seyahet[3][2][0]+seyahet[0][0][1]+seyahet[2][0][4]+seyahet[0][2][8]+seyahet[1][4][8]+" "+seyahet[1][0][3]+seyahet[3][0][5]+seyahet[4][1][3]+
+seyahet[1][2][2]+" " +seyahet[2][1][0]+seyahet[0][1][0][1]+seyahet[0][3][2]+seyahet[1][3][3]+seyahet[3][0][5]+seyahet[4][1][3]+seyahet[3][2][5]+
+seyahet[4][1][4])
+
+
+
